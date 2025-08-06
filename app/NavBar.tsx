941adbe61a158cd8c202cca002938d47b369eae6
Reyhaneh Zarrin <reyhaneh.zarrin@gmail.com>
2025-08-06 12:48:20 +0430

    set up

---
+++ app/NavBar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from "next/link";
+import React from "react";
+import { CgDebug } from "react-icons/cg";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  //array of objects
+  const links = [
+    { label: "dashboard", href: "/" },
+    { label: "issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <CgDebug />
+      </Link>
+      <ul className="flex space-x-6">
+        {/* We named every object link */}
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={
+              (classNames(
+                "tracking-normal",
+                {
+                "text-zinc-900": currentPath !== link.href,
+                "text-zinc-500": currentPath === link.href,
+                "hover:text-zinc-800": true,
+              }))
+            }
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
